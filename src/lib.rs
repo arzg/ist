06f64da703f3c56e858cb87e50b20f4ea0669a0a
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-10 12:39:09 +1000

    Display paths relative to the listing directory

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,30 @@
-use std::{
-    fmt,
-    path::{Path, PathBuf},
-};
+use std::{fmt, path::PathBuf};
 
 pub struct Listing {
-    paths: Vec<PathBuf>,
+    root: PathBuf,
+    child_paths: Vec<PathBuf>,
 }
 
 impl Listing {
-    pub fn new(path: impl AsRef<Path>) -> anyhow::Result<Self> {
-        let paths: Result<Vec<_>, _> = std::fs::read_dir(path)?
+    pub fn new(root: PathBuf) -> anyhow::Result<Self> {
+        let paths: Result<Vec<_>, _> = std::fs::read_dir(&root)?
             .map(|result| result.map(|dir_entry| dir_entry.path()))
             .collect();
 
         let paths = paths?;
 
-        Ok(Self { paths })
+        Ok(Self {
+            root,
+            child_paths: paths,
+        })
     }
 }
 
 impl fmt::Display for Listing {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for path in &self.paths {
-            writeln!(f, "{}", path.display())?;
+        for path in &self.child_paths {
+            // This strip_prefix call cannot fail because all the child paths start from root.
+            writeln!(f, "{}", path.strip_prefix(&self.root).unwrap().display())?;
         }
 
         Ok(())
